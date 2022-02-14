919581f09f7ef41cb6c715ac1d8b3d4e7bf4f4b3
Tomas Barak <baryhoemail@gmail.com>
2022-02-14 18:45:55 +0100

    add first midi-ledstrip interoperability

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,58 @@
 #include "mbed.h"
-#include "USBSerial.h"
- 
-//Virtual serial port over USB
-USBSerial serial;
-//DigitalOut test(LED_RED);
+#include "USBMIDI.h"
+
+BusOut statusLeds(LED_RED, LED_GREEN, LED_BLUE);
+USBMIDI midi;
 SPI ledStrip(/* mosi */P0_27, /* miso */P0_20, /* sclk */P0_24);
 
 const auto NumLeds = 60;
 const char startFrame[] = { 0x00, 0x00, 0x00, 0x00 };
 const char endFrame[] = { 0xff, 0xff, 0xff, 0xff };
 char pixels[NumLeds * 4];
+auto noteCounter = 0;
 
-int main(void) {
-	//test = 1;
-	//ledStrip.format(8, 0);
-	ledStrip.frequency(512000);
-	memset(pixels, 0, sizeof(pixels));
-	for (auto i = 0; i < NumLeds; i++)
+void midiCallback(MIDIMessage msg)
+{
+	switch (msg.type())
 	{
-		auto* led = pixels + i * 4;
-		led[0] = 0xe0 + 0x01;
-		led[1] = 255 * i / (NumLeds - 1);
-		led[2] = 255 - led[1];
+		case MIDIMessage::NoteOnType:
+			statusLeds[0] = 0;
+			++noteCounter;
+			break;
+		case MIDIMessage::NoteOffType:
+			statusLeds[0] = 1;
+			break;
+		default:;
 	}
-	
+}
+
+int main(void)
+{
+	midi.attach(midiCallback);
+	ledStrip.frequency(512000);
+	statusLeds = -1;
 
 	while(1)
 	{
-		//test = !test;
+		memset(pixels, 0, sizeof(pixels));
+		for (auto i = 0; i < NumLeds; i++)
+		{
+			auto* led = pixels + i * 4;
+			led[0] = 0xe0 + 0x01;
+			led[1] = 255 * i / (NumLeds - 1);
+			led[2] = 255 - led[1];
+		}
+
+		auto* led = pixels + (noteCounter % NumLeds) * 4;
+		led[0] = 0xe0 + 0x10;
+		led[1] = 0;
+		led[2] = 0;
+		led[3] = 255;
+
 		ledStrip.lock();
 		ledStrip.write(startFrame, sizeof(startFrame), nullptr, 0);
 		ledStrip.write(pixels, sizeof(pixels), nullptr, 0);
 		ledStrip.write(endFrame, sizeof(endFrame), nullptr, 0);
 		ledStrip.unlock();
-		//serial.printf("I am a virtual serial port 13.2.2022\r\n");
-		//wait(1);
 	}
 }
