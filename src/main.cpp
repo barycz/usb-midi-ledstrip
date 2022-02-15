0884442dbdc3adf704506951635fc73bea613ccb
Tomas Barak <baryhoemail@gmail.com>
2022-02-15 19:14:51 +0100

    another working example

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include "rtos/rtos.h"
 #include "USBMIDI.h"
 
 BusOut statusLeds(LED_RED, LED_GREEN, LED_BLUE);
@@ -10,6 +11,8 @@ const char startFrame[] = { 0x00, 0x00, 0x00, 0x00 };
 const char endFrame[] = { 0xff, 0xff, 0xff, 0xff };
 char pixels[NumLeds * 4];
 auto noteCounter = 0;
+bool noteHistory[NumLeds];
+auto midiClock = 0;
 
 void midiCallback(MIDIMessage msg)
 {
@@ -17,13 +20,29 @@ void midiCallback(MIDIMessage msg)
 	{
 		case MIDIMessage::NoteOnType:
 			statusLeds[0] = 0;
+			noteHistory[0] = true;
 			++noteCounter;
 			break;
 		case MIDIMessage::NoteOffType:
 			statusLeds[0] = 1;
 			break;
+		case MIDIMessage::SysExType:
+			if (msg.data[1] == 0xf8)
+			{
+				++midiClock;
+			}
+			break;
 		default:;
 	}
+}
+
+void updateHistory()
+{
+	for (auto i = NumLeds - 1; i > 0; --i)
+	{
+		noteHistory[i] = noteHistory[i - 1];
+	}
+	noteHistory[0] = false;
 }
 
 int main(void)
@@ -31,28 +50,40 @@ int main(void)
 	midi.attach(midiCallback);
 	ledStrip.frequency(512000);
 	statusLeds = -1;
+	memset(noteHistory, 0, sizeof(noteHistory));
 
 	while(1)
 	{
+		if (midiClock >= 6) // 24 per quater note
+		{
+			updateHistory();
+			midiClock = 0;
+		}
+
 		memset(pixels, 0, sizeof(pixels));
 		for (auto i = 0; i < NumLeds; i++)
 		{
 			auto* led = pixels + i * 4;
-			led[0] = 0xe0 + 0x01;
-			led[1] = 255 * i / (NumLeds - 1);
-			led[2] = 255 - led[1];
+			if (noteHistory[i])
+			{
+				led[0] = 0xe0 + 0x10;
+				led[1] = 0;
+				led[2] = 0;
+				led[3] = 255;
+			}
+			else
+			{
+				led[0] = 0xe0 + 0x01;
+				led[1] = 255 * i / (NumLeds - 1);
+				led[2] = 255 - led[1];
+			}
 		}
-
-		auto* led = pixels + (noteCounter % NumLeds) * 4;
-		led[0] = 0xe0 + 0x10;
-		led[1] = 0;
-		led[2] = 0;
-		led[3] = 255;
 
 		ledStrip.lock();
 		ledStrip.write(startFrame, sizeof(startFrame), nullptr, 0);
 		ledStrip.write(pixels, sizeof(pixels), nullptr, 0);
 		ledStrip.write(endFrame, sizeof(endFrame), nullptr, 0);
 		ledStrip.unlock();
+		//wait_ms(16);
 	}
 }
